1233a3b6982e7d9c4b69990e950f185f11bc0a07
Himalaya <yugmani@yahoo.com>
2021-04-03 00:43:08 -0700

    room name in chat room:ADD

--- chat/src/components/Chat.js
+++ chat/src/components/Chat.js
@@ -9,15 +9,28 @@ import {
 } from "@material-ui/icons";
 import MicIcon from "@material-ui/icons/Mic";
 import { useParams } from "react-router-dom";
+import db from "../firebase";
 
 const Chat = () => {
   const [input, setInput] = useState("");
   const { roomId } = useParams();
-
+  const [roomName, setRoomName] = useState("");
   const [seed, setSeed] = useState("");
+
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => {
+          // console.log(snapshot.data().name);
+          setRoomName(snapshot.data().name);
+        });
+    }
+  }, [roomId]);
 
   const sendMessage = (e) => {
     e.preventDefault();
@@ -29,7 +42,7 @@ const Chat = () => {
       <div className="chat__header">
         <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
-          <h3>Room name</h3>
+          <h3>{roomName}</h3>
           <p>Last seen at ... </p>
         </div>
         <div className="chat__headerRight">
